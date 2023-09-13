2c0edb339d6ce07f0e471277d5b4e5fab36cddb5
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-09-13 19:39:34 -0300

    feat: Configuring logging

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import resources.documentation
 import resources.external_api
 import resources.orders
-
-from app import flask_settings, database
-
+from app import database, flask_settings, log
 
 if __name__ == "__main__":
+    log.start_log()
+
+    log.add_message("Starting the database environment")
     database.setup_database_environment()
+
+    log.add_message("Starting Flask Settings")
+    log.add_message("")
     flask_settings.run_aplication()
