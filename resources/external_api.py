905e5b4eb1f7294324af521781b96502036bd8c0
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-09-13 21:48:36 -0300

    feat: Adding log

--- resources/external_api.py
+++ resources/external_api.py
@@ -3,7 +3,7 @@ from urllib.parse import unquote
 import requests
 from flask_openapi3 import Tag
 
-from app import app
+from app import app, log
 from schemas.external_api import (ExternalApiResultSchema, ExternalApiSchema,
                                   SingleMessageSchema)
 
@@ -27,9 +27,13 @@ def query_viacep_external_api(query: ExternalApiSchema):
     """
     External API query route ViaCep
     """
+    log.add_message("Get_viacep route accessed")
+
     zip_code = unquote(unquote(query.zip_code))
 
     try:
+        log.add_message("Querying viacep external API")
+
         url = f"https://viacep.com.br/ws/{zip_code}/json/"
         response = requests.get(url)
 
@@ -41,6 +45,20 @@ def query_viacep_external_api(query: ExternalApiSchema):
         if data.get("erro"):
             raise Exception("Zip code not found")
 
-        return {"message": "Success", "data": data}, 200
+        log.add_message("API consulted successfully")
+
+        return_data = {"message": "Success", "data": data}
+
+        log.add_message(f"Get_viacep response: {return_data}")
+        log.add_message("Get_viacep status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Get_viacep response: {return_data}")
+        log.add_message("Get_viacep status: 400")
+        log.add_message("")
+
+        return return_data, 400
--- resources/orders.py
+++ resources/orders.py
@@ -1,7 +1,7 @@
 import requests
 from flask_openapi3 import Tag
 
-from app import app, database
+from app import app, database, log
 from database.model.orders import Orders
 from schemas.orders import (
     MessageOrderSchema,
@@ -26,7 +26,11 @@ TAG_ORDERS = Tag(
 )
 def get_sales_order():
     """Get sales orders that are open via the online store api."""
+    log.add_message("Get_sales_order route accessed")
+
     try:
+        log.add_message("Accessing Online Store container to get sales")
+
         get_sales_url = "http://online-store-microservice:5000/get_sales"
         response_get_sales = requests.get(get_sales_url)
         response_sales_data = response_get_sales.json()
@@ -36,6 +40,8 @@ def get_sales_order():
                 response_sales_data["message"].replace("Error: ", "")
             )
 
+        log.add_message("Sales achieved")
+
         sales_data = response_sales_data.get("sales", [])
 
         address_columns = {
@@ -44,6 +50,9 @@ def get_sales_order():
             "street": "logradouro",
             "neighborhood": "bairro",
         }
+
+        log.add_message("Checking empty address fields")
+
         updated_sales_data = []
 
         for sale in sales_data:
@@ -56,6 +65,12 @@ def get_sales_order():
                 "Brazil",
                 "Brasil",
             ]:
+                log.add_message(f"Sale {sale['sales_id']} is incomplete")
+                log.add_message(
+                    f'Empty colums: {", ".join(empty_address_columns)}'
+                )
+                log.add_message("")
+
                 get_viacep_url = "http://127.0.0.1:5001/get_viacep/?zip_code="
                 zip_code = sale.get("zip_code", "")
                 response_get_viacep = requests.get(
@@ -68,15 +83,30 @@ def get_sales_order():
                     and len(data_viacep) > 0
                 ):
                     for empty_column in empty_address_columns:
-                        sale[empty_column] = (
-                            data_viacep[address_columns[empty_column]]
+                        new_data = data_viacep[address_columns[empty_column]]
+                        sale[empty_column] = new_data
+
+                        log.add_message(
+                            f"{empty_column} updated with {new_data}"
                         )
 
             updated_sales_data.append(sale)
 
-        return {"message": "Success", "sales_data": updated_sales_data}, 200
+        return_data = {"message": "Success", "sales_data": updated_sales_data}
+
+        log.add_message(f"Get_sales_order response: {return_data}")
+        log.add_message("Get_sales_order status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Get_sales_order: {return_data}")
+        log.add_message("Get_sales_order: 400")
+        log.add_message("")
+
+        return return_data, 400
 
 
 @app.post(
@@ -89,7 +119,11 @@ def get_sales_order():
 )
 def add_order():
     """Adds all updated sales order information."""
+    log.add_message("Add_order route accessed")
+
     try:
+        log.add_message("")
+
         get_order_url = "http://127.0.0.1:5001/get_sales_order"
         response_get_order = requests.get(get_order_url)
         response_order_data = response_get_order.json()
@@ -102,7 +136,12 @@ def add_order():
         orders_data = response_order_data.get("sales_data", {})
         added_orders = []
 
+        log.add_message("Adding orders")
+
         for order in orders_data:
+            log.add_message("")
+            log.add_message(f"Sale {order['sales_id']}")
+
             new_order = Orders(
                 name=order.get("name", ""),
                 price=order.get("price", ""),
@@ -124,6 +163,9 @@ def add_order():
             added_orders.append(formatted_response)
             database.insert_data_table(new_order)
 
+            log.add_message("Order added")
+            log.add_message("Accessing Online Store container to close sale")
+
             close_sale_url = "http://online-store-microservice:5000/close_sale"
             sales_id = {"sales_id": int(order["sales_id"])}
             response_close_sale = requests.put(close_sale_url, data=sales_id)
@@ -134,9 +176,23 @@ def add_order():
                     close_sale_data["message"].replace("Error: ", "")
                 )
 
-        return {"message": "Added Orders", "orders": added_orders}, 200
+            log.add_message("Closed sale")
+
+        return_data = {"message": "Added Orders", "orders": added_orders}
+
+        log.add_message(f"Add_order response: {return_data}")
+        log.add_message("Add_order status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Add_order response: {return_data}")
+        log.add_message("Add_order status: 400")
+        log.add_message("")
+
+        return return_data, 400
 
 
 @app.get(
@@ -149,7 +205,11 @@ def add_order():
 )
 def get_pending_invoices():
     """Get orders with pending invoice status."""
+    log.add_message("Get_pending_invoices route accessed")
+
     try:
+        log.add_message("Checking if there are pending orders")
+
         status_pending = "Pending"
         pending_orders = database.select_data_table(
             table=Orders,
@@ -158,6 +218,8 @@ def get_pending_invoices():
 
         if len(pending_orders) == 0:
             raise Exception("There are no pending orders")
+
+        log.add_message(f"There are {len(pending_orders)} pending orders")
 
         content = []
 
@@ -172,9 +234,24 @@ def get_pending_invoices():
 
             content.append(order_data)
 
-        return {"message": "There are pending orders", "orders": content}, 200
+        return_data = {
+            "message": "There are pending orders",
+            "orders": content
+        }
+
+        log.add_message(f"Get_pending_invoices response: {return_data}")
+        log.add_message("Get_pending_invoices status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Get_pending_invoices response: {return_data}")
+        log.add_message("Get_pending_invoices status: 400")
+        log.add_message("")
+
+        return return_data, 400
 
 
 @app.put(
@@ -187,6 +264,8 @@ def get_pending_invoices():
 )
 def complete_order(form: OrderCloseSchema):
     """Closes a order from the orders table."""
+    log.add_message("Complete_order route accessed")
+
     order_id = form.order_id
 
     try:
@@ -194,16 +273,24 @@ def complete_order(form: OrderCloseSchema):
             column=Orders.order_id, filter_select={Orders.order_id: order_id}
         )
 
+        log.add_message(f"Checking if order {order_id} exists")
+
         if not registered_order:
             raise Exception(f"The order {order_id} does not exist")
+
+        log.add_message("The order exists")
 
         invoice_status = database.select_value_table_parameter(
             column=Orders.invoice_status,
             filter_select={Orders.order_id: order_id}
         )
 
+        log.add_message(f"Checking if order {order_id} is completed")
+
         if invoice_status == "Completed":
             raise Exception(f"The order {order_id} is already completed")
+
+        log.add_message("The order is pending and will be finalized")
 
         new_invoice_status = "Completed"
         database.update_data_table(
@@ -212,6 +299,18 @@ def complete_order(form: OrderCloseSchema):
             new_data={Orders.invoice_status: new_invoice_status},
         )
 
-        return {"message": f"Order {order_id} completed successfully"}, 200
+        return_data = {"message": f"Order {order_id} completed successfully"}
+
+        log.add_message(f"Complete_order response: {return_data}")
+        log.add_message("Complete_order status: 200")
+        log.add_message("")
+
+        return return_data, 200
     except Exception as error:
-        return {"message": f"Error: {error}"}, 400
+        return_data = {"message": f"Error: {error}"}
+
+        log.add_message(f"Complete_order response: {return_data}")
+        log.add_message("Complete_order status: 400")
+        log.add_message("")
+
+        return return_data, 400
